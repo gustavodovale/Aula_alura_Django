867038645b3101d9a2aa55548d7d256b5f6962b3
gustavo <gust.dovale@gmail.com>
2024-01-18 14:00:57 -0300

    nomes dinamicos

--- galeria/views.py
+++ galeria/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-
+dados = {
+    1:{"nome": "Nebulosa de Carina",
+        "legenda": "webbtelescope.org / NASA / James Webb"},
+    2:{"nome":"Galáxia NGC 1079",
+        "legenda":"nasa.org / NASA / Hubble"}
+ }
 def index(request):
-    return render(request, 'galeria/index.html')
+    return render(request, 'galeria/index.html' , {"cards": dados})
 
+def imagem (request): 
+    return render(request, 'galeria/imagem.html')
