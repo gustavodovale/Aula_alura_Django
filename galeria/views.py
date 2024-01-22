8afacc8ecdd6c78015ebc7195061389d563a8c27
gustavo <gust.dovale@gmail.com>
2024-01-22 14:28:06 -0300

    acessando banco

--- galeria/views.py
+++ galeria/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
-dados = {
-    1:{"nome": "Nebulosa de Carina",
-        "legenda": "webbtelescope.org / NASA / James Webb"},
-    2:{"nome":"Galáxia NGC 1079",
-        "legenda":"nasa.org / NASA / Hubble"}
- }
+
+from galeria.models import Fotografia
+
 def index(request):
-    return render(request, 'galeria/index.html' , {"cards": dados})
+    fotografias = Fotografia.objects.all()
+    return render(request, 'galeria/index.html' , {"cards": fotografias})
 
 def imagem (request): 
     return render(request, 'galeria/imagem.html')
