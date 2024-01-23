d98f97f604c362de276c690f78b761b9983d71a1
gustavo <gust.dovale@gmail.com>
2024-01-23 14:36:19 -0300

    admin e categoria

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
-# Register your models here.
+from galeria.models import Fotografia
+class ListandoFotografias(admin.ModelAdmin):
+     list_display = ("id", "nome", "legenda" , "categoria")
+     list_display_links = ("id","nome")
+     search_fields = ("nome",)
+admin.site.register(Fotografia , ListandoFotografias)
